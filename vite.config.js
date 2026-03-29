f30f2899c53abb5a91014c7400ce76ad3db239f7
Nikhil Raj A <nikhilrajnair.a@gmail.com>
2026-03-29 16:37:59 +0200

    chore: remove unused formatting options from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import { defineConfig } from 'vite-plus';
 export default defineConfig({
   ignorePatterns: ['dist/**', '.angular/**', 'node_modules/**'],
 
-  semi: true,
-  singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  trailingComma: 'all',
-
   staged: {
     '*.ts': 'vp check --fix',
     '*.{json,html}': 'vp fmt',
